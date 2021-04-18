bc7120fee551a899709c8fcf634cc2cb72a6e05b
Abhimanyu <iamabhimanyu08@gmail.com>
2021-04-18 16:50:39 +0530

    completed task 3_5

--- project/1.py
+++
@@ -1,9 +0,0 @@
-import minitorch
-
-a = minitorch.Parameter(minitorch.rand((2,3), backend= minitorch.make_tensor_backend(minitorch.FastOps)))
-b = minitorch.Parameter(minitorch.rand((3,4), backend= minitorch.make_tensor_backend(minitorch.FastOps)))
-
-
-c = a.value@b.value
-
-print(c)
